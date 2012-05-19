ed750ed5be1214b6a4517386b22f56ba1e4b9e2d
Mariko Honma <maripiyo_k@yahoo.co.jp>
2012-05-19 21:51:12 +0900

    auto compileモードの導入

--- init.el
+++ init.el
@@ -12,7 +12,19 @@
 			(normal-top-level-add-subdirs-to-load-path))))))
 
 ;; 引数のディレクトリとそのサブディレクトリload-pathに追加
-(add-to-load-path "elisp" "conf" "public_repos")
+(add-to-load-path "elisp" "conf" "public_repos" "site-lisp")
+
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/site-lisp/ac-dict")
+(ac-config-default)
+
+
+;; auto-complete settings
+(when (require 'auto-complete-config nil t)
+  (add-to-list 'ac-dictionary-directories
+			   "~/.emacs.d/elisp/ac-dict")
+  (define-key ac-mode-map (kbd "M-TAB") 'auto-complete)
+  (ac-config-default))
 
 (require 'install-elisp)
 (setq install-elisp-repository-directory "~/.emacs.d/elisp/")
@@ -22,7 +34,7 @@
   ;; set install directory
   (setq auto-install-directory "~/.emacs.d/elisp")
   ;; Emacswikiに登録されているelispの名前を取得する
-  (auto-install-update-emacswiki-package-name t)
+  ;;(auto-install-update-emacswiki-package-name t)
   ;; install-elisp の関数を利用可能にする
   (auto-install-compatibility-setup))
 
@@ -93,3 +105,71 @@ nil 'japanese-jisx0208
 ;; git front end Egg setting
 (when (executable-find "git")
   (require 'egg nil t))
+
+;; load other configs
+(load "conf/init-groovy")
+(load "conf/init-tcl")
+(load "conf/init-org-mode")
+(load "conf/smart-compile-conf")
+
+;; anything
+;; (auto-install-batch "anything")
+(when (require 'anything nil t)
+  (setq
+   ;; 候補を表示するまでの時間。デフォルト0.5
+   anything-idle-delay 0.3
+   ;; タイプして再描写するまでの時間。デフォルト0.1
+   anything-input-idle-delay 0.2
+   ;; 候補の最大表示数。デフォルト50
+   anything-candidate-number-limit 100
+   ;; 候補が多い時に、体感速度を早くする
+   anything-quick-update t
+   ;; 候補選択ショートカットをアルファベットに
+   anything-enable-shortcut 'alphabet)
+
+  (require 'anything-match-plugin nil t)
+
+  (when (require 'anything-complete nil t)
+	;; lispシンボルの補完候補の再検索時間
+	(anything-lisp-complete-symbol-set-timer 150))
+
+  (require 'anything-show-completion nil t)
+
+  (when (require 'auto-install nil t)
+	(require 'anything-auto-install nil t))
+
+  (when (require 'descbinds-anything nil t)
+	(descbinds-anything-install))
+)
+
+;;; elisp setting
+;;; 試行錯誤用ファイルを開くための設定
+(require 'open-junk-file)
+;; C-x C-zで試行錯誤用ファイルを開く
+(global-set-key (kbd "C-x C-z") 'open-junk-file)
+;;; 式の評価結果を注釈するための設定
+(require 'paredit)
+(add-hook 'emacs-lisp-mode-hook 'enable-paredit-mode)
+(add-hook 'lisp-interaction-mode-hook 'enable-paredit-mode)
+(add-hook 'lisp-mode-hook 'enable-paredit-mode)
+(add-hook 'ielm-mode-hook 'enable-paredit-mode)
+(require 'auto-async-byte-compile)
+;; 自動バイトコンパイルを無効にするファイル名の正規表現
+(setq auto-async-byte-compile-exclude-files-regexp "/junk/")
+(add-hook 'emacs-lisp-mode-hook 'enable-auto-async-byte-compile-mode)
+(add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
+(add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode)
+(add-hook 'ielm-mode-hook 'turn-on-eldoc-mode)
+(setq eldoc-idle-delay 0.2)
+(setq eldoc-minor-mode-string "")
+;; 釣り合いの取れる括弧のハイライト
+(show-paren-mode 1)
+;; 改行と同時にインデントも行う
+(global-set-key "\C-m" 'newline-and-indent)
+;; find-functionキーを割り当てる
+(find-function-setup-keys)
+
+;;; windmove
+;;(windmove-default-keybindings 'meta) ; Alt の場合は meta を指定
+;; Mac の Command + 矢印でウィンドウを移動する
+(windmove-default-keybindings 'super)
